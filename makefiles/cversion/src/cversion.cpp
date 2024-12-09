36180934774e023926693354614641ef1f7196a0
PeterTaylor2 <33470226+PeterTaylor2@users.noreply.github.com>
2024-12-09 01:21:49 +0000

    executable to print the compiler version number - included in spi top-level solutions as well

---
+++ makefiles/cversion/src/cversion.cpp
@@ -0,0 +1,31 @@
+/*
+    Prints the compiler version number.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+#ifdef _MSC_VER
+    std::cout << "_MSC_VER=" << _MSC_VER << std::endl;
+#endif
+
+#ifdef __GNUC__
+    std::cout << "GNUC_VERSION=" << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << std::endl;
+#endif
+}
+
