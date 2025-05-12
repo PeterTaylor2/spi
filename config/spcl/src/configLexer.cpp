c9ce2ce987df1a65724f5ec536e28a392033dbea
PeterTaylor2 <33470226+PeterTaylor2@users.noreply.github.com>
2025-05-12 23:33:55 +0100

    regenerated configLexer.cpp with new copyright and license - no change in the rules yet

--- config/spcl/src/configLexer.cpp
+++ config/spcl/src/configLexer.cpp
@@ -593,18 +593,21 @@ static const flex_int32_t yy_rule_can_match_eol[59] =
 #define YY_RESTORE_YY_MORE_OFFSET
 /*
 
-Copyright (C) 2015 Sartorial Programming Ltd.
+    Sartorial Programming Interface (SPI) code generators
+    Copyright (C) 2012-2023 Sartorial Programming Ltd.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 /*
