2df1a03639aa4e5479200ed77737fadda00f21ca
PeterTaylor2 <33470226+PeterTaylor2@users.noreply.github.com>
2025-07-31 12:35:04 +0100

    in the spi::Value based version of ObjectUpdateMetaData copy the object first - this is because the low-level version does not copy the object when updating meta_data - otherwise Excel will get very confused

--- spi/src/InputContext.cpp
+++ spi/src/InputContext.cpp
@@ -1178,7 +1178,10 @@ Value ObjectUpdateMetaData(
     const Value& v25,
     const InputContext* context)
 {
-    ObjectConstSP object = context->ValueToObject(objectHandle, nullptr, true);
+    // Since ObjectUpdateMetaData (below) amends object (by modified the
+    // non-const meta_data field) we have to copy the object before we start.
+    // Otherwise excel gets very confused.
+    ObjectConstSP object = ObjectCopy(objectHandle, context).getObject();
 
     Value values[25] = { v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
         v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25 };
